06737c4b42ac894ffbdd3990eef859b0dcf6ef44
IJTINK7 <ijtink7@mail.ru>
2023-06-08 16:16:40 +0300

    Creating Header.tsx component, connecting it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./site-01/Header";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header/>
     </div>
   );
 }
