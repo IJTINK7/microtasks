60c5a1fea9fb1a7fefbc2882536b8898e27281d7
IJTINK7 <ijtink7@mail.ru>
2023-06-08 16:19:41 +0300

    Creating Body.tsx component, connecting it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./site-01/Header";
+import {Body} from "./site-01/Body";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Body/>
     </div>
   );
 }
