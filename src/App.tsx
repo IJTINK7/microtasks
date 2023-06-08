c301f9ad526e11cd692fa1b173aaec99dcd24472
IJTINK7 <ijtink7@mail.ru>
2023-06-08 16:22:30 +0300

    Creating Footer.tsx component, connecting it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./site-01/Header";
 import {Body} from "./site-01/Body";
+import {Footer} from "./site-01/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Body/>
+      <Footer/>
     </div>
   );
 }
